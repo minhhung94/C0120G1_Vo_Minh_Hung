18f898cf4b59a04f9152c18da243338c35afed0e
Minh Hùng Võ <nguyengiacat1@gmail.com>
2020-06-02 13:40:56 +0700

    22

---
+++ C0120G1_Vo_Minh_Hung/Module4/back-end/rent-building/src/test/java/com/example/rentbuilding/RentBuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentbuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentBuildingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
